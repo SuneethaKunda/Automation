7bdb2f675a1d05b383c848e10bb34ff3cf3c2c8b
Suneetha <Suneetha@Gunda>
2019-05-03 12:09:32 -0400

    Getrequest

--- APIautomation/src/main/java/postmanclient/Getrequest.java
+++ APIautomation/src/main/java/postmanclient/Getrequest.java
@@ -12,32 +12,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.testng.Assert;
+
+import postmanbase.Testbase;
 
 public class Getrequest{
 	
-	
+
 	//Get Method
-	public void get(String Geturl) throws ClientProtocolException, IOException {
+	public CloseableHttpResponse get(String Geturl) throws ClientProtocolException, IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet(Geturl);
 		CloseableHttpResponse httpresponse = httpclient.execute(httpget);
-	//Status code
-	int statuscode = httpresponse.getStatusLine().getStatusCode();
-	System.out.println(statuscode);
-	
-	//Json String
-	String responsestring = EntityUtils.toString(httpresponse.getEntity(),"UTF-8");
-	String Responsestring = responsestring.substring(2);
-	JSONObject jsonresponse = new JSONObject(Responsestring);
-	System.out.println("JSON Response"  +jsonresponse);
-	
-	//Headers
-	Header[] headersarray = httpresponse.getAllHeaders();
-	HashMap<String,String> allheaders =new HashMap<String,String>();
-	 for(Header header : headersarray ) {
-		 allheaders.put(header.getName(),header.getValue());
-		  }
-	 System.out.println("Headers  " +allheaders );
-	
-	}
+	return httpresponse;
+}
 }
--- APIautomation/src/test/java/postmanclienttest/Getrequesttest.java
+++ APIautomation/src/test/java/postmanclienttest/Getrequesttest.java
@@ -1,8 +1,14 @@
 package postmanclienttest;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,22 +18,44 @@ import postmanclient.Getrequest;
 public class Getrequesttest extends Testbase {
 
 	Testbase testbase;
+	
 	String geturl;
 	String getrequesturl;
-	
-	
+	CloseableHttpResponse httpresponse;
+
 	@BeforeMethod
-	public void setup()
-{
-		testbase =new Testbase();
-		
-		geturl= prop.getProperty("getrequesturl");
-		
-		}
+	public void setup() {
+		testbase = new Testbase();
+         geturl = prop.getProperty("getrequesturl");
+	}
 
 	@Test
 	public void gettest() throws ClientProtocolException, IOException {
 		Getrequest getrequest = new Getrequest();
 		getrequest = new Getrequest();
-		getrequest.get(geturl);
-	}}
+	CloseableHttpResponse httpresponse=	getrequest.get(geturl);
+		//Status code
+		int statuscode = httpresponse.getStatusLine().getStatusCode();
+		System.out.println(statuscode);
+		Assert.assertEquals(statuscode,RESPOSE_STATUS_CODE_200,"Status is not equals to 200");
+		
+		
+		//Json String
+		String responsestring = EntityUtils.toString(httpresponse.getEntity(),"UTF-8");
+		String Responsestring = responsestring.substring(2);
+		JSONObject jsonresponse = new JSONObject(Responsestring);
+		System.out.println("JSON Response"  +jsonresponse);
+		
+		//Headers
+		Header[] headersarray = httpresponse.getAllHeaders();
+		HashMap<String,String> allheaders =new HashMap<String,String>();
+		 for(Header header : headersarray ) {
+			 allheaders.put(header.getName(),header.getValue());
+			  }
+		 System.out.println("Headers  " +allheaders );
+		
+		}
+	
+		
+	}
+
